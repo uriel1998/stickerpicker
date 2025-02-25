663ca10e831bb9dafec0a13fbf0156a4cfb2bae2
Steven Saus <steven@stevesaus.com>
2025-02-25 07:15:16 -0500

    redid server

--- sticker/version.py
+++ sticker/version.py
@@ -1,1 +1,6 @@
-from .get_version import git_tag, git_revision, version, linkified_version
+# Generated in setup.py
+
+git_tag = None
+git_revision = '89d3aece'
+version = '0.1.0+dev.89d3aece'
+linkified_version = '0.1.0+dev.[89d3aece](https://github.com/maunium/stickerpicker/commit/89d3aece041c85ebe5a1ad4e620388af5227cbb0)'
